946cacaeb47716964f2db637e883873cb2b36685
NukeTurtle <estiglic@edocuments.co.uk>
2022-12-03 23:55:44 +0000

    add Books array

--- functions.js
+++ functions.js
@@ -1,7 +1,26 @@
-myLibrary = [{
+const myLibrary  = [];
+
+const myBook = {
+    bookId: "",
     bookTitle: "",
     author: "",
     numberOfPages: "",
     bookRead: false,
     releaseYear: ""
-}]
+}
+
+
+function addBook(bookId, bookTitle, author, numberOfPages, releaseYear) {
+    var newBook = new Object()
+    newBook.bookId = bookId,
+    newBook.bookTitle = bookTitle,
+    newBook.author = author,
+    newBook.numberOfPages = numberOfPages,
+    newBook.bookRead = false,
+    newBook.releaseYear = releaseYear
+    myLibrary.push(newBook)
+    console.log("New Book: ", myLibrary)
+    localStorage.setItem("Books", JSON.stringify(myLibrary))
+}
+
+
