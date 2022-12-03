0f2852d9999baa16bf99ad9c38678fd1e0cf58ad
steelsnake93 <nikolanikotasev@protonmail.com>
2022-12-03 19:42:28 +0100

    added two dummy books to the array

--- functions.js
+++ functions.js
@@ -1,7 +1,23 @@
-myLibrary = [{
-    bookTitle: "",
-    author: "",
-    numberOfPages: "",
+var books = [{
+    bookTitle: "Harry Potter and the Goblet of Fire",
+    author: "J. K. Rowling",
+    numberOfPages: "464",
     bookRead: false,
-    releaseYear: ""
-}]
+    releaseYear: "2019"
+},
+{
+    bookTitle: "Harry Potter and the Deathly Hallows",
+    author: "J. K. Rowling",
+    numberOfPages: "759",
+    bookRead: true,
+    releaseYear: "2007"
+}];
+
+
+function Book() {
+    // the constructor...
+}
+
+function addBookToLibrary() {
+    // do stuff here
+} 
