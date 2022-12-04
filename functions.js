6c5272f0b923ac707a83a309a9dfded07046c414
NukeTurtle <estiglic@edocuments.co.uk>
2022-12-04 02:01:14 +0000

    add Book constructor, fix addBookTo Library and myLibrary

--- functions.js
+++ functions.js
@@ -1,49 +1,51 @@
+//     {
+//         bookTitle: "Harry Potter and the Goblet of Fire",
+//         author: "J. K. Rowling",
+//         numberOfPages: "464",
+//         bookRead: false,
+//         releaseYear: "2019"
+//     },
+//     {
+//         bookTitle: "Harry Potter and the Deathly Hallows",
+//         author: "J. K. Rowling",
+//         numberOfPages: "759",
+//         bookRead: true,
+//         releaseYear: "2007"
+//     }
 
-var books = [{
-    bookTitle: "Harry Potter and the Goblet of Fire",
-    author: "J. K. Rowling",
-    numberOfPages: "464",
-    bookRead: false,
-    releaseYear: "2019"
-},
-{
-    bookTitle: "Harry Potter and the Deathly Hallows",
-    author: "J. K. Rowling",
-    numberOfPages: "759",
-    bookRead: true,
-    releaseYear: "2007"
-}];
+class Book {
+    constructor(bookId, bookTitle, bookAuthor, bookPages, bookRead, bookYear) {
+        this.id = bookId,
+        this.title = bookTitle,
+        this.author = bookAuthor,
+        this.pages = bookPages,
+        this.read = bookRead,
+        this.year = bookYear
 
-
-function Book() {
-    // the constructor...
+        // this.myBook = function() {
+        //     return this.bookId + this.title + this.author + this.pages + this.read + this.year;
+        // };
+    }
 }
-
-function addBookToLibrary() {
-    // do stuff here
-} 
-
-const myLibrary  = [];
-
-const myBook = {
-    bookId: "",
-    bookTitle: "",
-    author: "",
-    numberOfPages: "",
-    bookRead: false,
-    releaseYear: ""
+const myLibrary = [];
+const currentLibrary = [];
+console.log(currentLibrary)
+function addBookToLibrary(bookId, bookTitle, bookAuthor, bookPages, bookRead, bookYear) {
+    let newBook = new Book(bookId, bookTitle, bookAuthor, bookPages, bookRead, bookYear)
+    // newBook.id = bookId,
+    // newBook.title = bookTitle,
+    // newBook.author = bookAuthor,
+    // newBook.pages = bookPages,
+    // newBook.read = false,
+    // newBook.year = bookYear
+    var currentLibrary = myLibrary + myLibrary.push(newBook)
+    // console.log("New Book: ", addBook.name)
+    // localStorage.setItem("Books", JSON.stringify(currentLibrary))
+    console.log("current lib",currentLibrary)
+    return myLibrary;
 }
+console.log("current lib 2",currentLibrary)
 
-
-function addBook(bookId, bookTitle, author, numberOfPages, releaseYear) {
-    var newBook = new Object()
-    newBook.bookId = bookId,
-    newBook.bookTitle = bookTitle,
-    newBook.author = author,
-    newBook.numberOfPages = numberOfPages,
-    newBook.bookRead = false,
-    newBook.releaseYear = releaseYear
-    myLibrary.push(newBook)
-    console.log("New Book: ", myLibrary)
-    localStorage.setItem("Books", JSON.stringify(myLibrary))
-}
+// TODO:
+// -store myLibrary in local Storage
+// -check why Book is always added in console
